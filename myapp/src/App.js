c056e6563ab656b1432160085b7cbc52e4215e2f
Eklavya18100 <98822142+Eklavya18100@users.noreply.github.com>
2023-08-08 06:37:49 +0000

    DOne

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,175 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+try {
+  const [inputEValue, setInputEValue] = useState('');
+  const [inputDValue, setInputDValue] = useState('');
+
+  const handleEChange = (event) => {
+    setInputEValue(event.target.value);
+  };
+
+  const encrypt = () => {
+
+    let obj = {
+      A: 'hh',
+      B: 'hm',
+      C: 'mh',
+      D: 'mm',
+      E: 'hhh',
+      F: 'hhm',
+      G: 'hmh',
+      H: 'hmm',
+      I: 'mhh',
+      J: 'mhm',
+      K: 'mmh',
+      L: 'mmm',
+      M: 'hhhh',
+      N: 'hhhm',
+      O: 'hhmh',
+      P: 'hhmm',
+      Q: 'hmhh',
+      R: 'hmhm',
+      S: 'hmmh',
+      T: 'hmmm',
+      U: 'mhhh',
+      V: 'mhhm',
+      W: 'mhmh',
+      X: 'mhmm',
+      Y: 'mmhh',
+      Z: 'mmhm',
+      a: 'hh',
+      b: 'hm',
+      c: 'mh',
+      d: 'mm',
+      e: 'hhh',
+      f: 'hhm',
+      g: 'hmh',
+      h: 'hmm',
+      i: 'mhh',
+      j: 'mhm',
+      k: 'mmh',
+      l: 'mmm',
+      m: 'hhhh',
+      n: 'hhhm',
+      o: 'hhmh',
+      p: 'hhmm',
+      q: 'hmhh',
+      r: 'hmhm',
+      s: 'hmmh',
+      t: 'hmmm',
+      u: 'mhhh',
+      v: 'mhhm',
+      w: 'mhmh',
+      x: 'mhmm',
+      y: 'mmhh',
+      z: 'mmhm'
+    };
+
+    let ans = "", w = "";
+    let p = inputEValue.split(' ');
+
+    for (let i = 0; i < p.length; i++) {
+      
+      for (let j = 0; j < p[i].length; j++) {
+        // console.log(p[i][j]);
+        if(w.length==0)w +=obj[p[i][j]];
+        else
+        w += "."+obj[p[i][j]];
+      }
+      ans =ans+ w + " ";
+      w = "";
+    }
+    // console.log(p);
+
+    return ans;
+
+  }
+
+  const handleDChange = (event) => {
+    setInputDValue(event.target.value);
+  };
+
+  const decrypt = () => {
+    let encryptedValue=inputDValue;
+    let obj = {
+      hh: 'a',
+      hm: 'b',
+      mh: 'c',
+      mm: 'd',
+      hhh: 'e',
+      hhm: 'f',
+      hmh: 'g',
+      hmm: 'h',
+      mhh: 'i',
+      mhm: 'j',
+      mmh: 'k',
+      mmm: 'l',
+      hhhh: 'm',
+      hhhm: 'n',
+      hhmh: 'o',
+      hhmm: 'p',
+      hmhh: 'q',
+      hmhm: 'r',
+      hmmh: 's',
+      hmmm: 't',
+      mhhh: 'u',
+      mhhm: 'v',
+      mhmh: 'w',
+      mhmm: 'x',
+      mmhh: 'y',
+      mmhm: 'z'
+    };
+  
+    let ans = "";
+    let words = encryptedValue.split(' ');
+  
+    for (let i = 0; i < words.length; i++) {
+      let letters = words[i].split('.');
+      let w="";
+      for (let j = 0; j < letters.length; j++) {
+        {
+          w += obj[letters[j]];
+        }
+      }
+      ans += w+" ";
+      
+    }
+  
+    return ans.trim();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>"Hmm machine"</h1>
+
+      <div className="container">
+        <h3>Encode</h3>
+        <div>
+          <input type="text" value={inputEValue} onChange={handleEChange} />
+          <p> Encrypted result:{encrypt()}</p>
+        </div>
+
+      </div>
+      <div className="container">
+        <h3>Decode</h3>
+        <div>
+          <input type="text" value={inputDValue} onChange={handleDChange} />
+          <p> Decrypted result:{decrypt()}</p>
+        </div>
+
+      </div>
+
     </div>
   );
+} catch (error) {
+  alert("I am a lazy developer. No conditon checks. Fix your shit coz my code won't be fixed...Hmm")
 }
+}
+
 
 export default App;
